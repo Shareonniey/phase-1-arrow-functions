9970dd8e05dfefffe4fe075085cc6ce80ae452a5
Shareonniey <sharonadhiambo015@gmail.com>
2025-09-16 06:46:12 +0300

    adds arrow functions lab

---
+++ index.js
@@ -0,0 +1,11 @@
+const add = (a,b) => a + b;
+add(2000,100);
+const divide = (d,e) => d / e ;
+divide(2000,100);
+console.log(divide(2000,100));
+function square (number){
+    return number * number;
+
+}
+let number = square (10);
+console.log (number);
